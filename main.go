feb634ff29e3029cf50aabf931bb6c057845d858
Michael Hrcek <michael@michaelhrcek.com>
2018-04-19 15:40:43 -0400

    Started working on story view/add char page template.

--- main.go
+++ main.go
@@ -38,25 +38,25 @@ func main(){
 	router.HandleFunc("/story/{storyuid:[0-9]{1,9}}/delete", testHandler) //delete story
 	router.HandleFunc("/story/{storyuid:[0-9]{1,9}}/edit", EditStoryHandler) //edit story
 	router.HandleFunc("/story/{storyuid:[0-9]{1,9}}/json", GetJSONStoryHandler) //get story json
-	
+
 		//chapter
 	router.HandleFunc("/story/{storyuid:[0-9]{1,9}}/{chapteruid:[0-9]{1,9}}", testHandler) //Chapter overview
 	router.HandleFunc("/story/{storyuid:[0-9]{1,9}}/new", NewChapterHandler) //new chapter
 	router.HandleFunc("/story/{storyuid:[0-9]{1,9}}/{chapteruid:[0-9]{1,9}}/delete", testHandler) //delete chapter
 	router.HandleFunc("/story/{storyuid:[0-9]{1,9}}/{chapteruid:[0-9]{1,9}}/edit", testHandler) //edit chapter
 	router.HandleFunc("/story/{storyuid:[0-9]{1,9}}/{chapteruid:[0-9]{1,9}}/json", testHandler) //get chapter json
-	
+
 		//section
 	router.HandleFunc("/story/{storyuid:[0-9]{1,9}}/{chapteruid:[0-9]{1,9}}/{sectionuid:[0-9]{1,9}}", testHandler) //Section Overview
 	router.HandleFunc("/story/{storyuid:[0-9]{1,9}}/{chapteruid:[0-9]{1,9}}/new", NewSectionHandler) //new section
 	router.HandleFunc("/story/{storyuid:[0-9]{1,9}}/{chapteruid:[0-9]{1,9}}/{sectionuid:[0-9]{1,9}}/delete", testHandler) //delete section
 	router.HandleFunc("/story/{storyuid:[0-9]{1,9}}/{chapteruid:[0-9]{1,9}}/{sectionuid:[0-9]{1,9}}/edit", testHandler) //edit section
 	router.HandleFunc("/story/{storyuid:[0-9]{1,9}}/{chapteruid:[0-9]{1,9}}/{sectionuid:[0-9]{1,9}}/json", testHandler) //get section json
-	
+
 		//overview
 	router.HandleFunc("/story/list", ListJSONStoryHandler) //Story list json
 	router.HandleFunc("/story", OverviewStoryHandler) //Story list
-	
+
 	http.ListenAndServe(":8080", router)
 }
 
